f15f48b33ec98aba329aa8cba5fbe6282791187a
Edward Beech <initialed85@gmail.com>
2024-05-11 17:05:46 +0800

    Add index page thing

--- build-for-prod.sh
+++ build-for-prod.sh
@@ -13,8 +13,8 @@ function teardown() {
 trap teardown exit
 pushd "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null 2>&1
 
-sed -i.bak s%ws://localhost:8081/ws%wss://quake.initialed85.cc/ws%g ../Quake-WASM/WinQuake/net_websocket.c
-sed -i.bak s%ws://localhost:8081/ws%wss://quake.initialed85.cc/ws%g ../Quake-WASM/WinQuake/Makefile.emscripten
+sed -i.bak s%ws://localhost:8081/ws%wss://quake-play.initialed85.cc/ws%g ../Quake-WASM/WinQuake/net_websocket.c
+sed -i.bak s%ws://localhost:8081/ws%wss://quake-play.initialed85.cc/ws%g ../Quake-WASM/WinQuake/Makefile.emscripten
 
 cd ../Quake-WASM
 docker build --platform=linux/amd64 -t kube-registry:5000/quake-wasm:latest -f ./Dockerfile .
@@ -25,6 +25,10 @@ docker build --platform=linux/amd64 -t kube-registry:5000/quake-server:latest -f
 cd ../quake-websocket-proxy
 docker build --platform=linux/amd64 -t kube-registry:5000/quake-websocket-proxy:latest -f ./Dockerfile .
 
+cd ./index
+docker build --platform=linux/amd64 -t kube-registry:5000/quake-index:latest -f ./Dockerfile .
+
 docker image push kube-registry:5000/quake-wasm:latest
 docker image push kube-registry:5000/quake-server:latest
 docker image push kube-registry:5000/quake-websocket-proxy:latest
+docker image push kube-registry:5000/quake-index:latest
